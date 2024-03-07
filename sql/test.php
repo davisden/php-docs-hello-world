e96cba08880b77bef55eb193d754a5b4968a2e9b
Denise S Davis <42788305+davisden@users.noreply.github.com>
2024-03-07 04:33:58 -0500

    Update test.php

--- sql/test.php
+++ sql/test.php
@@ -3,6 +3,7 @@
     try {
     $conn = new PDO("sqlsrv:server = tcp:itecazure.database.windows.net,1433; Database = demoitec24", "itecadmin", "Azure2024");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connecting";
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
