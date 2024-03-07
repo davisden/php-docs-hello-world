faf836caa59882871d53d99d34b192b5322b6696
Denise S Davis <42788305+davisden@users.noreply.github.com>
2024-03-07 04:00:36 -0500

    Update test.php

--- sql/test.php
+++ sql/test.php
@@ -1,14 +1,13 @@
 <?php
-    echo "Starting"
-    $serverName = "tcp:itecazure.database.windows.net,1433"; // update me
-    $connectionOptions = array(
-        "Database" => "demoitec24", // update me
-        "Uid" => "itecadmin", // update me
-        "PWD" => "Azure2024" // update me
-    );
-    //Establishes the connection
-    $conn = sqlsrv_connect($serverName, $connectionOptions);
-    $tsql= "SELECT  ProductId, ProductName, Price, ProductDescription
+    try {
+    $conn = new PDO("sqlsrv:server = tcp:itecazure.database.windows.net,1433; Database = demoitec24", "itecadmin", "");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+   $tsql= "SELECT  ProductId, ProductName, Price, ProductDescription
          FROM dbo.product";
     $getResults= sqlsrv_query($conn, $tsql);
     echo ("Reading data from table" . PHP_EOL);
