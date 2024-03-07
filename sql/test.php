ebae9bfe84017e9f552586e6735f6529ca994011
Denise S Davis <42788305+davisden@users.noreply.github.com>
2024-03-07 03:30:26 -0500

    Create test.php

---
+++ sql/test.php
@@ -0,0 +1,20 @@
+<?php
+    $serverName = "itecazure.database.windows.net"; // update me
+    $connectionOptions = array(
+        "Database" => "demoitec24", // update me
+        "Uid" => "itecadmin", // update me
+        "PWD" => "Azure2024" // update me
+    );
+    //Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    $tsql= "SELECT  ProductId, ProductName, Price, ProductDescription
+         FROM dbo.product";
+    $getResults= sqlsrv_query($conn, $tsql);
+    echo ("Reading data from table" . PHP_EOL);
+    if ($getResults == FALSE)
+        echo (sqlsrv_errors());
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+     echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
+    }
+    sqlsrv_free_stmt($getResults);
+?>
