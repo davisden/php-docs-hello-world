d9f3c0c5ba61540f14f924ef394f08b46134f7f3
Denise S Davis <42788305+davisden@users.noreply.github.com>
2024-03-07 04:48:18 -0500

    Update test.php

--- sql/test.php
+++ sql/test.php
@@ -1,14 +1,6 @@
-<?php
-    echo "Start";
-    try {
-    $conn = new PDO("sqlsrv:server = tcp:itecazure.database.windows.net,1433; Database = demoitec24", "itecadmin", "Azure2024");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connecting";
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
+<?php // test.php
+require_once 'login.php';
+ 
    $tsql= "SELECT  ProductId, ProductName, Price, ProductDescription
          FROM dbo.product";
     $getResults= sqlsrv_query($conn, $tsql);
