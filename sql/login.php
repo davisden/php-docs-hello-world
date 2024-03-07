52805c9c1632deb9b52863e8064f7743773fc1aa
Denise S Davis <42788305+davisden@users.noreply.github.com>
2024-03-07 05:37:38 -0500

    Update login.php

--- sql/login.php
+++ sql/login.php
@@ -1,14 +1,15 @@
 <?php
-    echo "Start";
+    echo "<h1>Start</h1>";
     $conn = new PDO("sqlsrv:server = tcp:itecazure.database.windows.net,1433; Database = demoitec24", "itecadmin", "Azure2024");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connecting";
-    echo "Fetching";
+    echo "<h2>Connected!!!</h2>";
+    echo "<h1>Fetching Data </h2>";
 $query = 'select * from dbo.Products';  
   
 // simple query  
 $stmt = $conn->query( $query );  
 while ( $row = $stmt->fetch( PDO::FETCH_ASSOC ) ){ 
-   echo $row['ProductID']." , ".$row['ProductName']." , ".$row['Price']." , ".$row['ProductDescription']."<br />";
+   echo "<br />".$row['ProductID']." , ".$row['ProductName']." , ".$row['Price']." , ".$row['ProductDescription'];
 }  
 ?>
+<h1> ALL DONE! </h1>
